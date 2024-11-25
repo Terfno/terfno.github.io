ae475dc7a8465714b7382c9b7bb25ab604e0cee3
terfno <terfno@cookpad.com>
2024-11-25 22:53:29 +0900

    :zap: add pay

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,10 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://sueda.jp",
+  redirects: {
+    '/pay': {
+      status: 308,
+      destination: 'https://scrapbox.io/terfno/pay'
+    }
+  }
 });
