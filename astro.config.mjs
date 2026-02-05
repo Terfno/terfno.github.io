5f727616da81735f7cb28c0f34ca72e46150be20
terfno <sueda.takahito@gmail.com>
2026-02-05 12:31:18 +0900

    :zap: update /pay

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://sueda.jp",
-  redirects: {
-    "/pay": {
-      status: 308,
-      destination: "https://scrapbox.io/terfno/pay",
-    },
-  },
   vite: {
     plugins: [tailwindcss()],
   },
